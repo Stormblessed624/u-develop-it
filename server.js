a304fd8d9acfa43576619c426843ebc482b3b760
Matt <matt.white.624@gmail.com>
2021-09-19 15:17:24 -0600

    connected database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,9 +8,18 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
-});
+// Connect to mysql database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'BrandonSanderson04!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 app.get('/', (req, res) => {
     res.json({
@@ -17,6 +27,16 @@ app.get('/', (req, res) => {
     });
 });
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+// Default response for any other request (Not Found), should be last route
 app.use((req, res) => {
     res.status(404).end();
+});
+
+// Creates PORT for localhost:3001
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
 });
